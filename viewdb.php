a34498831fa9d415eeb89e272cf58719c3d95048
HananMustafa <hananmustafaawan@gmail.com>
2024-08-14 12:12:41 +0500

    db setup

--- viewdb.php
+++ viewdb.php
@@ -16,18 +16,18 @@ if ($result->num_rows > 0) {
 }
 
 // Query to show rows from a specific table
-$table_name = "your_table_name";
-$sql = "SELECT * FROM $table_name";
-$result = $conn->query($sql);
+// $table_name = "your_table_name";
+// $sql = "SELECT * FROM $table_name";
+// $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        print_r($row);
-        echo "<br>";
-    }
-} else {
-    echo "No rows found";
-}
+// if ($result->num_rows > 0) {
+//     while($row = $result->fetch_assoc()) {
+//         print_r($row);
+//         echo "<br>";
+//     }
+// } else {
+//     echo "No rows found";
+// }
 
 $conn->close();
 ?>
